000e7cfda77d28700a58400fb54fff77e50fbdb3
Fred Hatfull <fred.hatfull@gmail.com>
2011-09-13 18:45:16 -0400

    Added models for preliminary database schema. Closes #2.

--- partify/models.py
+++ partify/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column
-from sqlalchemy import Integer
-from sqlalchemy import String
+import datetime
+
+from sqlalchemy import BigInteger, Column, DateTime, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import relationship
 
 from database import Base
 
@@ -18,3 +19,36 @@ class User(Base):
 
     def __repr__(self):
         return "<User %r>" % (self.name)
+
+class Track(Base):
+    __tablename__ = "track"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(Text)
+    artist = Column(Text)
+    album = Column(Text)
+    spotify_url = Column(Text, unique=True)
+
+    def __repr__(self):
+        return "<%r by %r (from %r) - %r>" % (self.title, self.artist, self.album, self.spotify_url)
+
+class PlayQueue(Base):
+    __tablename__ = "play_queue"
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    track_id = Column(Integer, ForeignKey('track.id'))
+    track = relationship("Track")
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User")
+    time_added = Column(DateTime, default=datetime.datetime.now)
+    user_priority = Column(Integer)
+    playback_priority = Column(BigInteger, unique=True)
+
+    def __repr__(self):
+        return "<Track %r queued by %r at %r with priority %r (queue position %r)>" % (self.track, self.user, self.time_added, self.user_priority, self.playback_priority)
+
+class ConfigurationField(Base):
+    __tablename__ = "configuration_field"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    field_name = Column(Text)
+    field_value = Column(Text)
