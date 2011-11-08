4d9cd7d2957e4c6141f06943ba8d83656266e31e
Fred Hatfull <fred.hatfull@gmail.com>
2011-11-08 17:53:12 -0500

    Default to externally visible server

--- partify/config.py
+++ partify/config.py
@@ -34,7 +34,7 @@ def load_config_from_db():
         'MPD_SERVER_PORT': 0,
         'SECRET_KEY': _produce_random_data(),
         'SERVER': 'builtin',
-        'SERVER_HOST': '127.0.0.1',
+        'SERVER_HOST': '0.0.0.0',
         'SERVER_PORT': 5000, 
         'SESSION_SALT': _produce_random_data()
     }
