e554639b93ad817bc3cc3a930e2fdc522e93f7ce
Fred Hatfull <fred.hatfull@gmail.com>
2011-11-10 18:00:12 -0500

    Maybe we want scripts?

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Fred Hatfull',
     author_email='fred.hatfull@gmail.com',
     packages=['partify'],
-    scripts=[],
+    scripts=['run.py'],
     url='http://www.partify.us',
     license='LICENSE.txt',
     description='Collaborative Spotify Music Streamer',
