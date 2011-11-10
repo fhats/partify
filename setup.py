c165456b5459a1ae2ee876e5eec0ba0b973bc643
Fred Hatfull <fred.hatfull@gmail.com>
2011-11-10 16:20:50 -0500

    Don't need scripts - hopefully these will come with the debian package.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Fred Hatfull',
     author_email='fred.hatfull@gmail.com',
     packages=['partify'],
-    scripts=['run.py'],
+    scripts=[],
     url='http://www.partify.us',
     license='LICENSE.txt',
     description='Collaborative Spotify Music Streamer',
