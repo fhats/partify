5ed42f409648277da0c7cf2eca36d3add31785c7
Fred Hatfull <fred.hatfull@gmail.com>
2011-11-10 19:56:16 -0500

    Error in manifest

--- bin/install_ubuntu.sh
+++ bin/install_ubuntu.sh
@@ -3,8 +3,8 @@ wget -q -O - http://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
 sudo wget -q -O /etc/apt/sources.list.d/mopidy.list http://apt.mopidy.com/mopidy.list
 sudo apt-get update
 sudo apt-get install mopidy -y
-sudo apt-get install python-setuptools -y
+sudo apt-get install python-pip -y
 
 
 # install partify
-sudo easy_install partify
+sudo pip install partify
