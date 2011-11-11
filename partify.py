70934e3e25004cb61f7b8bc53bcea3d0c6797acf
Fred Hatfull <fred.hatfull@gmail.com>
2011-11-11 13:37:04 -0500

    Fix up run script, move runner into init

--- partify.py
+++ partify.py
@@ -17,42 +17,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Partify.  If not, see <http://www.gnu.org/licenses/>."""
 
-import datetime
-
-from werkzeug.contrib.profiler import MergeStream
-from werkzeug.contrib.profiler import ProfilerMiddleware
-from werkzeug.serving import run_simple
-
-from partify import app
-from partify import ipc
-from partify import on_startup
-from partify.database import init_db
-
-# TODO: Figure out these imports
-from partify import admin, player, queue, track, user
 
 if __name__ == "__main__":
-    """Starts the WebApp."""
-    init_db()
-    on_startup()
-    
-    app.logger.debug(app.config)
-
-    if app.config.get('PROFILE', False):
-        datetime_string = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-        f = open("tmp/profile%s.log" % datetime_string, "w")
-        stream = MergeStream(f)
-        app.wsgi_app = ProfilerMiddleware(app.wsgi_app, stream)
-
-    if app.config['SERVER'] == 'builtin':
-        app.run(host=app.config['SERVER_HOST'], port=app.config['SERVER_PORT'])
-    elif app.config['SERVER'] == 'tornado':
-        import tornado.options
-        from tornado.wsgi import WSGIContainer
-        from tornado.httpserver import HTTPServer
-        from tornado.ioloop import IOLoop
-
-        tornado.options.enable_pretty_logging()
-        http_server = HTTPServer(WSGIContainer(app))
-        http_server.listen(app.config['SERVER_PORT'])
-        IOLoop.instance().start()
+    from partify import start
+    start()
