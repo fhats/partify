d2f934aa906ec9981eb92b25f105fc17c4703a9e
Fred Hatfull <fred.hatfull@gmail.com>
2011-11-11 16:47:46 -0500

    Perfecting the install_ubuntu script.

--- bin/install_ubuntu.sh
+++ bin/install_ubuntu.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
+# A quick and dirty force-feed script to get Partify and Mopidy up and running on Ubuntu systems
+
 # installs Mopidy
 wget -q -O - http://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
 sudo wget -q -O /etc/apt/sources.list.d/mopidy.list http://apt.mopidy.com/mopidy.list
@@ -5,7 +9,25 @@ sudo apt-get update
 sudo apt-get install mopidy -y
 sudo apt-get install python-setuptools -y
 
+echo "Enter your Spotify Premium username: "
+read spotify_username
+echo "Enter your Spotify Premium password: "
+stty -echo
+read spotify_password ; echo
+stty echo
+
+sudo mkdir /root/.local
+sudo mkdir /root/.local/share
+sudo mkdir /root/.local/share/mopidy
+sudo mkdir /root/.config
+sudo mkdir /root/.config/mopidy
+sudo rm /root/.config/mopidy/settings.py
+
+sudo echo "SPOTIFY_USERNAME = '$spotify_username'" >> /root/.config/mopidy/settings.py
+sudo echo "SPOTIFY_PASSWORD = '$spotify_password'" >> /root/.config/mopidy/settings.py
 
 # install partify
 sudo easy_install partify
-sudo partify.py
+
+# Run Partify!
+sudo run_partify
